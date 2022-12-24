e7916b0b1839ffd51223c35f574f2ccf89a21d2c
Ahmed Hemida <Ahmed-Hemida>
2022-12-24 16:53:24 +0200

    no message

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,11 @@
 #include <mpi.h> 
 #include<omp.h>
-#include <chrono>
 #include <stdio.h> 
 #include<fstream>
 #include<string>
+
 #include <iostream>
 using namespace std;
-using namespace std::chrono;
-
 #define N 5000000
 int main(int argc, char* argv[])
 {
@@ -16,10 +14,9 @@ int main(int argc, char* argv[])
 	char character;
 	char* arr = (char*)malloc(N * sizeof(char));
 
-	string word = "GCCAGATATTCCCCCCGTT";
-	int wordlen = 19;
-	int localcount = 0;
-	int totalcount = 0;
+	int localsize = 0;
+	int localcount[4] = { 0 };
+	int globalcount[4] = { 0 };
 
 	int rank;
 	int numofprocessors;
@@ -27,25 +24,26 @@ int main(int argc, char* argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numofprocessors);
 	MPI_Barrier(MPI_COMM_WORLD);
-	string filepath="C:\\Users\\pop\\source\\repos\\par\\";
 	if (rank == 0) {
+
+		string filepath = "C:\\Users\\pop\\source\\repos\\par\\";
+
 		//file.open(filepath+"DNA50.txt");
 		//file.open(filepath+"DNA500.txt");
 		//file.open(filepath+"DNA50000.txt");
-		file.open(filepath+"DNA5000000.txt");
+	   file.open(filepath+"DNA5000000.txt");
 
-		if (file.fail()) {
+		if (file.fail())
 			cout << " error failed to open file!!!" << endl;
-
-			cout << " path:%s" << filepath + "DNA5000000.txt" << endl;
-		}
 		else {
 			cout << " file opened successfully\n";
 
 			while (!file.eof()) {
 				if (x < N) {
 
-					file.get(arr[x]);
+					file.get(character);
+					arr[x] = character;
+					//printf("newarr[%d]=%c\n", x, newarr[x] );
 					x++;
 
 				}
@@ -54,12 +52,12 @@ int main(int argc, char* argv[])
 					break;
 				}
 			}
-
 		}
 		cout << "number of characters in file = " << x << endl;
+
 	}
 
-	int localsize = N / numofprocessors;
+	localsize = N / numofprocessors;
 	int remain = N % numofprocessors;
 	int* sendcount = new int[numofprocessors];
 	int recvcount;
@@ -69,44 +67,57 @@ int main(int argc, char* argv[])
 		for (int i = 0; i < numofprocessors; i++) {
 			sendcount[i] = localsize;
 			displics[i] = i * localsize;
-
+			recvcount = localsize;
 		}
-
-		recvcount = localsize;
-
 		char* recvbuff;
-		recvbuff = (char*)malloc(recvcount * sizeof(char));
+		recvbuff = (char*)malloc(localsize * sizeof(char));
 
 		MPI_Scatterv(arr, sendcount, displics, MPI_CHAR, recvbuff, recvcount, MPI_CHAR, 0, MPI_COMM_WORLD);
-		int i, j,thread_id, nthreads;
-		auto start = high_resolution_clock::now();
+
+		int nthreads, thread_id;
 #pragma omp parallel private (thread_id)
 		{
-			thread_id = omp_get_thread_num();
+
 			nthreads = omp_get_num_threads();
-		//	for (i = 0; i < recvcount; i++)
-		for(i=(thread_id * recvcount / nthreads); i < (thread_id + 1) * recvcount / nthreads; i++)
-			{
-				for (j = 0; j < wordlen; j++)
-				{
-					if (recvbuff[i + j] != word[j])
-						break;
-				}
-				if (j == wordlen)
-				{
-					localcount++;
-					//i += wordlen;
-				}
+			thread_id = omp_get_thread_num();
 
+			for (int i = (thread_id * sendcount[rank] / nthreads)-1; i < (thread_id + 1) * sendcount[rank] / nthreads; i++) {
+				//for (int i = 0; i < sendcount[0]; i++) {
+
+				if (recvbuff[i] == 'A')
+					localcount[0]++;
+
+				if (recvbuff[i] == 'C')
+					localcount[1]++;
+
+				if (recvbuff[i] == 'G')
+					localcount[2]++;
+
+				if (recvbuff[i] == 'T')
+					localcount[3]++;
 			}
+#pragma omp barrier    /* valid usage    */
 		}
-		auto stop = high_resolution_clock::now();
-		auto duration = duration_cast<microseconds>(stop - start);
-		cout << "Time taken by function: " << duration.count()/1000 << " mile seconds" << endl;
-		MPI_Reduce(&localcount, &totalcount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		for (int i = (nthreads*(sendcount[rank] / nthreads))+1; i < sendcount[rank] ; i++) {
+			//for (int i = 0; i < sendcount[0]; i++) {
 
+			if (recvbuff[i] == 'A')
+				localcount[0]++;
+
+			if (recvbuff[i] == 'C')
+				localcount[1]++;
+
+			if (recvbuff[i] == 'G')
+				localcount[2]++;
+
+			if (recvbuff[i] == 'T')
+				localcount[3]++;
+		}
+		MPI_Reduce(&localcount, &globalcount, 4, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 		if (rank == 0)
-			printf("total count of \"GCCAGATATTCCCCCCGTT\"=%d\n", totalcount);
+			printf("count of A=%d  \n count of C=%d \n count of G=%d \ncount of T=%d \n", globalcount[0], globalcount[1], globalcount[2], globalcount[3]);
+
+
 	}
 
 	if (remain != 0) {
@@ -119,36 +130,32 @@ int main(int argc, char* argv[])
 			displics[i] = (i * localsize) + remain;
 			recvcount = sendcount[i];
 		}
+
 		char* recvbuff;
-		recvcount += remain;
-		recvbuff = (char*)malloc(recvcount * sizeof(char));
-		MPI_Scatterv(arr, sendcount, displics, MPI_CHAR, recvbuff, recvcount, MPI_CHAR, 0, MPI_COMM_WORLD);
-		printf("displics[%d]=%d\n", rank, displics[rank]);
-		int i, j, thread_id=0, nthreads=0;
-		//#pragma omp parallel private (thread_id)
-		{
-			thread_id = omp_get_thread_num();
-			nthreads = omp_get_num_threads();
-				for (i = 0; i < recvcount; i++)
-			//for (i = (thread_id * recvcount / nthreads); i < (thread_id + 1) * recvcount / nthreads; i++) {
-				for (j = 0; j < wordlen; j++)
-				{
-					if (recvbuff[i + j] != word[j])
-						break;
-				}
-				if (j == wordlen)
-				{
-					localcount++;
+		recvbuff = (char*)malloc(localsize * sizeof(char));
 
-				}
-			}
+		MPI_Scatterv(arr, sendcount, displics, MPI_CHAR, recvbuff, recvcount + remain, MPI_CHAR, 0, MPI_COMM_WORLD);
+		for (int i = 0; i < sendcount[0]; i++) {
+
+			if (recvbuff[i] == 'A')
+				localcount[0]++;
+
+			if (recvbuff[i] == 'C')
+				localcount[1]++;
+
+			if (recvbuff[i] == 'G')
+				localcount[2]++;
+
+			if (recvbuff[i] == 'T')
+				localcount[3]++;
 		}
-
-		MPI_Reduce(&localcount, &totalcount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(&localcount, &globalcount, 4, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
 		if (rank == 0)
-			printf("total count of \"GCCAGATATTCCCCCCGTT\"=%d\n", totalcount);
-	
+			printf("count of A=%d \n count of C=%d \n count of G=%d\n count of T=%d \n", globalcount[0], globalcount[1], globalcount[2], globalcount[3]);
+
+	}
+
 	MPI_Finalize();
 
 	return 0;
